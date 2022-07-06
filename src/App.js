5e58a1b97e3c8701b649a8106cbd252c73ba8ab4
cham0287 <62415003+cham0287@users.noreply.github.com>
2022-07-06 22:13:57 +0900

    메인 화면 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import './App.css';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
-  return <div className='App'>hello</div>;
+  let [shoes, setShoes] = useState(data);
+
+  return (
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Features</Nav.Link>
+            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          {shoes.map((shoe, i) => {
+            return <Shoe i={i} shoe={shoe} />;
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+function Shoe(props) {
+  return (
+    <Col md={4}>
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+      />
+      <h4>{props.shoe.title}</h4>
+      <p>{props.shoe.content}</p>
+      <p>Price: {props.shoe.price}</p>
+    </Col>
+  );
 }
 
 export default App;
